fd76d82ad54174cbf3135cf1950d81231647aa12
Gustavo Ehrhardt <gus.ehr@gmail.com>
2016-10-14 22:53:45 -0300

    initial setup

---
+++ src/main/java/br/org/eufraten/manutencao/EufratenTrelloApplication.java
@@ -0,0 +1,12 @@
+package br.org.eufraten.manutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EufratenTrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EufratenTrelloApplication.class, args);
+	}
+}
