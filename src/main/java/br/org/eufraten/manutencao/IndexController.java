e4a95173cf22c746ff4a5782a325a8aae99904b7
Gustavo Ehrhardt <gus.ehr@gmail.com>
2016-10-14 23:19:07 -0300

    index controller

---
+++ src/main/java/br/org/eufraten/manutencao/IndexController.java
@@ -0,0 +1,15 @@
+package br.org.eufraten.manutencao;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+	@ResponseBody
+	public String ok() {
+		return "OK";
+	}
+}
