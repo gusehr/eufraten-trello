2751872489be5c9950439ff6566c5d67f89061ba
Gustavo Ehrhardt <gus.ehr@gmail.com>
2017-02-04 21:57:52 -0200

    lista de cartoes para gerar relatorio de OS

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,8 +1,9 @@
 const
 quadroManutencao = {
 	autenticacaoOK : false,
-	listGerarOS: "56eb606f6daa325d8468ad07",
-	
+	listGerarOS : "56eb606f6daa325d8468ad07",
+	cardExplicacaoGerarOS : "56ec3f240a9ce096cd473cd1",
+
 	init : function() {
 		Trello.authorize({
 			name : "eufraten-manutencao-geral",
@@ -30,9 +31,37 @@ quadroManutencao = {
 	},
 
 	cardsListados : function(cards) {
-		
-		
-		console.log(cards);
+		const
+		selectGerarOS = $('#select-gerarOS'), btnGerarOS = $("#btn-gerarOS");
+
+		selectGerarOS.empty();
+		btnGerarOS.attr("disabled", "disabled");
+
+		if (cards.length <= 1) {
+			selectGerarOS
+					.append($(
+							'<option>',
+							{
+								value : "",
+								text : "Nenhum cartão encontrado nas listas de ordens de serviço"
+							}));
+			return;
+		}
+
+		selectGerarOS.append($('<option>', {
+			value : "",
+			text : "Selecione o cartão"
+		}));
+		$.each(cards, function(i, card) {
+			if (card.id != quadroManutencao.cardExplicacaoGerarOS) {
+				selectGerarOS.append($('<option>', {
+					value : card.id,
+					text : card.name
+				}));
+			}
+		});
+
+		btnGerarOS.removeAttr("disabled");
 	},
 
 	erro : function(error) {
