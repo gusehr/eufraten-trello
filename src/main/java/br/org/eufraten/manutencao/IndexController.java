57aacfde2f9de5076e2723bd5d0dedc34eb8beb6
Gustavo Ehrhardt <gus.ehr@gmail.com>
2017-02-11 10:47:32 -0200

    listar release version

--- src/main/java/br/org/eufraten/manutencao/IndexController.java
+++ src/main/java/br/org/eufraten/manutencao/IndexController.java
@@ -2,6 +2,7 @@ package br.org.eufraten.manutencao;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -10,4 +11,15 @@ public class IndexController {
 	public String abrir() {
 		return "index";
 	}
+
+	@GetMapping("/versao")
+	@ResponseBody
+	public String versao() {
+		String versao = System.getenv("HEROKU_RELEASE_VERSION");
+		if (versao == null || versao.equals("")) {
+			versao = "SNAPSHOT";
+		}
+		return versao;
+	}
+
 }
